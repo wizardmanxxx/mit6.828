99178d04976c0bc21ace7d0a168b090b86a48193
wizardmanxxx <wxd5566@qq.com>
2018-08-22 11:16:14 +0800

    add backtrace function name

--- kern/monitor.c
+++ kern/monitor.c
@@ -24,6 +24,7 @@ struct Command
 static struct Command commands[] = {
 	{"help", "Display this list of commands", mon_help},
 	{"kerninfo", "Display information about the kernel", mon_kerninfo},
+	{"backtrace","Display the list of called function and stack",mon_backtrace}
 };
 #define NCOMMANDS (sizeof(commands) / sizeof(commands[0]))
 
@@ -65,12 +66,20 @@ int mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 
 		int *eip = (int *)nebp + 1;
 		ebp = (int *)(*nebp);
+
+		struct Eipdebuginfo *info;
+
+		debuginfo_eip(eip,info);
+
 		int *arg0 = eip + 1;
 		int *arg1 = arg0 + 1;
 		int *arg2 = arg1 + 1;
 		int *arg3 = arg2 + 1;
 		int *arg4 = arg3 + 1;
 		cprintf("ebp %08x eip %08x args %08x %08x %08x %08x %08x\n", *ebp, *eip, *arg0, *arg1, *arg2, *arg3, *arg4);
+		int fn_off = eip - info->eip_fn_addr;
+		cprintf("%s:c:%d %s+%d",info->eip_file,info->eip_line,info->eip_fn_name,fn_off);
+
 		nebp = ebp;
 	}
 
